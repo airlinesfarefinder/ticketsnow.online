d0a4d1402053aca2762b6545ef967f618d6e90c9
airlinesfarefinder <airlinesfarefinder@gmail.com>
2025-07-19 22:13:35 +0530

    Updete

--- inc/config.php
+++ inc/config.php
@@ -6,7 +6,7 @@ function config($key)
     static $configuration = [
         'phone_number' => '(888) 497-2660',
         'email_id' => 'info@ticketsnow.online',
-        'address' => 'demo, Boston, MA 02109, United States',
+        'address' => '23 Hawkins Street Boston, MA 02114',
         'domain_name' => 'ticketsnow.online',
     ];
 
